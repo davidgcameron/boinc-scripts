9d203c7e59535ea161eb67e205779d860f2d967b
David Cameron <d.g.cameron@gmail.com>
2019-12-09 16:10:13 +0100

    small bash fixes

--- native/run_atlas.sh
+++ native/run_atlas.sh
@@ -18,7 +18,7 @@
 
 # Redirect output to stderr with timestamp
 log() {
-  IFS=''
+  local IFS=''
   while read line; do
     d=$(date)
     echo "$d: $line" 1>&2
@@ -39,7 +39,7 @@ echo "Arguments: $@"
 
 # Get number of threads
 threads=1
-if [ "$1" == "--threads" ]; then
+if [ "$1" = "--threads" ]; then
   threads=$2
 fi
 
@@ -53,22 +53,18 @@ fi
 
 # Check CVMFS
 echo "Checking for CVMFS"
-which cvmfs_config &>/dev/null
-if [ $? -ne 0 ]; then
+if ! which cvmfs_config &>/dev/null; then
   echo "No cvmfs_config command found, will try listing directly"
-  timeout 60 ls -d /cvmfs/atlas.cern.ch/repo/sw > /dev/null
-  if [ $? -ne 0 ]; then
+  if ! timeout 60 ls -d /cvmfs/atlas.cern.ch/repo/sw > /dev/null; then
     echo "Failed to list /cvmfs/atlas.cern.ch/repo/sw"
     cleanexit 1
   fi
 else
-  cvmfs_config probe
-  if [ $? -ne 0 ]; then
+  if ! cvmfs_config probe; then
     echo "cvmfs_config probe failed, aborting the job"
     cleanexit 1
   fi
-  cvmfs_config stat atlas.cern.ch
-  if [ $? -ne 0 ]; then
+  if ! cvmfs_config stat atlas.cern.ch; then
     echo "cvmfs_config stat atlas.cern.ch failed, aborting the job"
     cleanexit 1
   fi
@@ -78,12 +74,10 @@ echo "CVMFS is ok"
 # Check singularity executable
 sin_binary="/cvmfs/atlas.cern.ch/repo/containers/sw/singularity/x86_64-el7/current/bin/singularity"
 echo "Checking for singularity binary..."
-sin_location=$(which singularity)
-if [ $? -eq 0 ]; then
+if sin_location=$(which singularity); then
   echo "Using singularity found in PATH at ${sin_location}"
   echo "Running ${sin_location} --version"
-  ${sin_location} --version
-  if [ $? -ne 0 ]; then
+  if ! ${sin_location} --version; then
     echo "Singularity seems to be installed but not working"
     echo "Will use version from CVMFS"
   else
@@ -95,12 +89,11 @@ fi
 
 # Check singularity works
 echo "Checking singularity works with ${sin_binary} exec -B /cvmfs ${sin_image} hostname"
-output=$(${sin_binary} exec -B /cvmfs ${sin_image} hostname 2>&1)
-if [ $? -ne 0 ]; then
-  if [[ "${sin_binary}" == "/cvmfs/"* ]] && [ `grep -c "Failed to create user namespace" ${output}` == "1" ]; then
+if ! output=$(${sin_binary} exec -B /cvmfs ${sin_image} hostname 2>&1); then
+  if [[ "${sin_binary}" == "/cvmfs/"* ]] && [ $(echo "$output" | grep -c "Failed to create user namespace") == "1" ]; then
     echo 'It looks like user namespaces are not enabled, which are required when running singularity from CVMFS.'
     echo 'Please run the following command as root to enable them:'
-    echo ' echo "user.max_user_namespaces = 15000" > /etc/sysctl.d/90-max_user_namespaces.conf; sysctl -p /etc/sysctl.d/90-max_user_namespaces.conf\n'
+    echo ' echo "user.max_user_namespaces = 15000" > /etc/sysctl.d/90-max_user_namespaces.conf; sysctl -p /etc/sysctl.d/90-max_user_namespaces.conf'
   fi
   echo "Singularity isnt working: ${output}"
   cleanexit 1
