021dd21f8f2779dd3ccf801cfc77b951227aaf6e
computezrmle <57127745+computezrmle@users.noreply.github.com>
2022-11-23 16:13:03 +0100

    Update ATLASbootstrap.sh
    make function 'early_exit' work with longer delays.

--- vbox/ATLASbootstrap.sh
+++ vbox/ATLASbootstrap.sh
@@ -11,45 +11,53 @@ function early_exit {
     # on a modern multi CPU computer many tasks often start concurrently
     # if they fail ensure they shut down after a random delay to flatten load peaks
     sleep $(shuf -n 1 -i 720-900)
-    echo 0 > /home/atlas/ATLASJobAgent.pid
-    sudo shutdown now
+
+    if findmnt "$shared_dir" >/dev/null 2>&1; then
+        sudo sh -c "echo -e \"206\n206\n[ERROR] Early shutdown while executing initial bootstrap.\" >${shared_dir}/atlas_done"
+    else
+        sudo shutdown now
+    fi
+    
+    exit 0
 }
 
 
 function probe_atlas_repo {
-    echo "[INFO] Probing /cvmfs/atlas.cern.ch..." | vboxmonitor
     cvmfs_config probe atlas.cern.ch >${my_tmp_dir}/probe_atlas_log 2>&1
     probe_atlas_ret=$?
-    echo "[INFO] $(cat ${my_tmp_dir}/probe_atlas_log)"
+    echo "[INFO] $(cat ${my_tmp_dir}/probe_atlas_log)" | vboxmonitor
     if [ "$probe_atlas_ret" -ne "0" ]; then
         early_exit
     fi
 }
 
 
+# clear the proxy setup at every VM restart
+sudo sh -c "rm -f /var/www/html/wpad.dat"
+
 umask_bak="$(umask)"
 umask 077
 my_tmp_dir="$(mktemp -d)"
 umask "${umask_bak}"
+export shared_dir="/home/atlas/shared"
+mkdir -p "$shared_dir"
 
-# clear the proxy setup at every VM restart
-sudo sh -c "rm -f /var/www/html/wpad.dat"
-
+# probe only atlas instead of all repos listed in default.local
 # This can be time consuming
 # do it in the bg
+echo "[INFO] Probing /cvmfs/atlas.cern.ch..." | vboxmonitor
 probe_atlas_repo &
 bg_probe_atlas_repo=$!
 
-
 echo "[INFO] Mounting shared directory" | vboxmonitor
-sudo mount -t vboxsf shared /home/atlas/shared
+sudo mount -t vboxsf shared "$shared_dir"
 if [ "$?" -ne "0" ]; then
     echo "[DEBUG] Failed to mount shared directory" | vboxmonitor
     early_exit
 fi
   
 echo "[INFO] Checking for init_data.xml" | vboxmonitor
-init_data="/home/atlas/shared/init_data.xml"
+init_data="${shared_dir}/init_data.xml"
 if [ ! -f $init_data ]; then
     echo "[DEBUG] init_data.xml can't be found" | vboxmonitor
     early_exit
@@ -64,15 +72,14 @@ else
     jobwrapper_name="ATLASJobWrapper-prod.sh"
 fi
 
-rm -f /home/atlas/ATLASJobWrapper.sh
 cp /cvmfs/atlas.cern.ch/repo/sw/BOINC/agent/${jobwrapper_name} /home/atlas/ATLASJobWrapper.sh
 if [ "$?" -ne "0" ]; then
     echo "[DEBUG] Failed to copy ${jobwrapper_name}" | vboxmonitor
     early_exit
 fi
-
 chown atlas:atlas /home/atlas/ATLASJobWrapper.sh
 chmod +x /home/atlas/ATLASJobWrapper.sh
+
 
 /home/atlas/ATLASJobWrapper.sh &
 echo $! > /home/atlas/ATLASJobAgent.pid
